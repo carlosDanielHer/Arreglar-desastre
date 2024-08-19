9d5015f549f600efb81c7af7f9430635499fb159
Carlos Daniel Hernandez Duran <profesionalengineer123@gmail.com>
2024-08-19 09:51:33 -0400

    feat(ve.techcare): Agregar el Look and Feel al Login desde el main
    - Agregar la interfaz de Login a la clase main para que se ejecute desde ella.
    - Agregar el Look and Feel en la clase main para que se aplique a la interfaz del Login.

--- src/main/java/ve/techcare/TechCare.java
+++ src/main/java/ve/techcare/TechCare.java
@@ -1,5 +1,9 @@
 package ve.techcare;
 
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import ve.techcare.vistas.Login;
+
 /**
  *
  * @author Carlos Hernandez
@@ -7,6 +11,22 @@ package ve.techcare;
 public class TechCare {
 
     public static void main(String[] args) {
-       
+       try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Crear y mostrar el JFrame
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Login().setVisible(true);
+            }
+        });
     }
 }
