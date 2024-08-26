1776f1666a6b93029bdfd68f1a5de139b396067f
Carlos Daniel Hernandez Duran <profesionalengineer123@gmail.com>
2024-08-26 16:41:00 -0400

    fix (ve.techcare.vistas): Cambiar la forma de cerrar CambioContraseña
    - Cambiar la forma en la cual la interfaz CambioContraseña cierra, pero esta vez cuando el usuario da clic en el icono de cerrar por defecto de al interfaz y no despues de actualizar la contraseña.

--- src/main/java/ve/techcare/vistas/CambioContraseña.java
+++ src/main/java/ve/techcare/vistas/CambioContraseña.java
@@ -43,7 +43,7 @@ public class CambioContraseña extends javax.swing.JFrame {
         nuevaContraseña_txt = new javax.swing.JTextField();
         restablecer_btt = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
         jPanel1.setPreferredSize(new java.awt.Dimension(495, 524));
