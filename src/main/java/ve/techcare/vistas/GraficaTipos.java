d3844d7c5d4f9097c449931f7b5f72c797ebae90
Carlos Daniel Hernandez Duran <profesionalengineer123@gmail.com>
2024-09-05 07:38:26 -0400

    feat(ve.techcare.vistas): Agregar labels  título y footer en GraficaTipos
    - Agregar JLabel titulo_lb y footer_lb en la clase GraficaTipos.
    - Configurar propiedades de fuente y color para titulo_lb y footer_lb.
    - Añadir titulo_lb y footer_lb a panelFondo con posiciones absolutas.

--- src/main/java/ve/techcare/vistas/GraficaTipos.java
+++ src/main/java/ve/techcare/vistas/GraficaTipos.java
@@ -21,12 +21,24 @@ public class GraficaTipos extends javax.swing.JFrame {
     private void initComponents() {
 
         panelFondo = new javax.swing.JPanel();
+        titulo_lb = new javax.swing.JLabel();
+        footer_lb = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         panelFondo.setBackground(new java.awt.Color(255, 255, 255));
         panelFondo.setPreferredSize(new java.awt.Dimension(1040, 665));
         panelFondo.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        titulo_lb.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        titulo_lb.setForeground(new java.awt.Color(0, 0, 0));
+        titulo_lb.setText("Grafica de los Equipos según sus Tipos");
+        panelFondo.add(titulo_lb, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 10, 450, 40));
+
+        footer_lb.setFont(new java.awt.Font("Segoe UI", 1, 13)); // NOI18N
+        footer_lb.setForeground(new java.awt.Color(0, 0, 0));
+        footer_lb.setText("TechCare® System ");
+        panelFondo.add(footer_lb, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 630, 240, 20));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -82,7 +94,9 @@ public class GraficaTipos extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel footer_lb;
     private javax.swing.JPanel panelFondo;
+    private javax.swing.JLabel titulo_lb;
     // End of variables declaration//GEN-END:variables
 
 }
