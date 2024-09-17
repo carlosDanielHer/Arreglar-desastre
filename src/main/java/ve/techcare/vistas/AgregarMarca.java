ca1bcc71685a8f068f9c39cc520f83197f3d33ba
Carlos Daniel Hernandez Duran <profesionalengineer123@gmail.com>
2024-09-17 09:38:04 -0400

    feat(ve.techcare.vistas): Agregar componentes a AgregarMarca
    - Agregar etiquetas titulo_lb, agregarMarca_lb, footer_lb.
    - Agregar campo de texto agregarMarca_txt.
    - Agregar botón acceder_btt con su ActionListener.
    - Configurar fuente, posición y color de letra de los componentes.

--- src/main/java/ve/techcare/vistas/AgregarMarca.java
+++ src/main/java/ve/techcare/vistas/AgregarMarca.java
@@ -23,12 +23,45 @@ public class AgregarMarca extends javax.swing.JFrame {
     private void initComponents() {
 
         panelFondo = new javax.swing.JPanel();
+        titulo_lb = new javax.swing.JLabel();
+        agregarMarca_lb = new javax.swing.JLabel();
+        agregarMarca_txt = new javax.swing.JTextField();
+        acceder_btt = new javax.swing.JButton();
+        footer_lb = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         panelFondo.setBackground(new java.awt.Color(255, 255, 255));
         panelFondo.setPreferredSize(new java.awt.Dimension(393, 489));
         panelFondo.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        titulo_lb.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        titulo_lb.setForeground(new java.awt.Color(0, 0, 0));
+        titulo_lb.setText("Agregar Marca");
+        panelFondo.add(titulo_lb, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 0, 180, 30));
+
+        agregarMarca_lb.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        agregarMarca_lb.setForeground(new java.awt.Color(0, 0, 0));
+        agregarMarca_lb.setText("Agrega el Marca de equipo aquí");
+        panelFondo.add(agregarMarca_lb, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 240, -1, -1));
+
+        agregarMarca_txt.setFont(new java.awt.Font("Segoe UI", 1, 13)); // NOI18N
+        panelFondo.add(agregarMarca_txt, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 260, 320, 60));
+
+        acceder_btt.setFont(new java.awt.Font("Segoe UI", 1, 20)); // NOI18N
+        acceder_btt.setForeground(new java.awt.Color(0, 0, 0));
+        acceder_btt.setText("REGISTRAR");
+        acceder_btt.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                acceder_bttActionPerformed(evt);
+            }
+        });
+        panelFondo.add(acceder_btt, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 360, 150, 60));
+
+        footer_lb.setFont(new java.awt.Font("Segoe UI", 1, 13)); // NOI18N
+        footer_lb.setForeground(new java.awt.Color(0, 0, 0));
+        footer_lb.setText("TechCare® System ");
+        panelFondo.add(footer_lb, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 460, 200, 20));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -47,6 +80,10 @@ public class AgregarMarca extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void acceder_bttActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_acceder_bttActionPerformed
+        
+    }//GEN-LAST:event_acceder_bttActionPerformed
 
     /**
      * @param args the command line arguments
@@ -84,7 +121,12 @@ public class AgregarMarca extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton acceder_btt;
+    private javax.swing.JLabel agregarMarca_lb;
+    private javax.swing.JTextField agregarMarca_txt;
+    private javax.swing.JLabel footer_lb;
     private javax.swing.JPanel panelFondo;
+    private javax.swing.JLabel titulo_lb;
     // End of variables declaration//GEN-END:variables
 
 }
